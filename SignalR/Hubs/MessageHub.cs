8eedd7d3c86bd53d3a055d3bba6a401311684b20
dangthai228 <ngodangthai22820001@gmail.com>
2022-12-21 14:43:55 +0700

    Init

---
+++ SignalR/Hubs/MessageHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalR.Hubs
+{
+    public class MessageHub : Hub
+    {
+        public async Task Send(string user, string message)
+        {
+            await Clients.All.SendAsync("Message", user, message);  
+        }
+    }
+}
